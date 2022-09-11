53b021ed13d6a1a649dc6d0058980242ab0a4b49
Tejas Rajendra Ahirrao <101903576+tejasahirrao1103@users.noreply.github.com>
2022-09-11 14:38:45 +0530

    Update rockpapercessor.py

--- rockpapercessor.py
+++ rockpapercessor.py
@@ -1,6 +1,24 @@
-def code_not_available():
-    print("""sorry!!!....... 
-    
-the program of this game is not avilabele yet. 
-    
-    """)
+def rpccode():
+    import random
+    from re import X
+
+    def rpc():
+        while True:
+            n=int(input("""
+    choose your option 
+
+    1. rock 
+    2. paper
+    3. sesor
+    """))
+            x=random.randint(0,3)
+            if n==x:
+                print("compute and you bot are tie...")
+            if n!=x:
+                
+                if (n==1 and x==3) or (n==2 and x==1) or (n==3 and x==2):
+                    print("you win bro")
+                else:
+                    print("opps.. computer win")
+
+    rpc()
